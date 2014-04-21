a1be511c49f1c1fb4b5364fcdb3bafd78bc2ab85
Wolfram Kriesing <wk@uxebu.com>
2014-04-21 14:01:36 +0200

    Just better grouping.

--- test/promiseSpec.js
+++ test/promiseSpec.js
@@ -20,33 +20,35 @@ function functionThatCallsMockedDependency(externalDependency) {
 }
 
 describe('promises and jasmine', function() {
-  it('the simplest passing promise test', function(done) {
-    promiseThatResolves().then(success).done();
+  describe('resolving promises', function() {
+    it('the simplest', function(done) {
+      promiseThatResolves().then(success).done();
 
-    function success() {
-      expect(true).toBe(true);
-      done();
-    }
-    // Issues here:
-    // - missing `done();` on the last line makes the tests run looong (5s) until the timeout catches
-    //   that would be acceptable, since it kinda gives a hint where to look
-    // - to have the function success at all makes the flow of the test just a bit harder to read
-  });
-
-  it('a bit better passing promise test', function(done) {
-    var success = jasmine.createSpy('success');
-    promiseThatResolves().then(success).done();
-
-    success.andCallFake(function() {
-      expect(success).toHaveBeenCalled();
-      done();
+      function success() {
+        expect(true).toBe(true);
+        done();
+      }
+      // Issues here:
+      // - missing `done();` on the last line makes the tests run looong (5s) until the timeout catches
+      //   that would be acceptable, since it kinda gives a hint where to look
+      // - to have the function success at all makes the flow of the test just a bit harder to read
     });
-    // Pro
-    // - removes the little meaningful `expect(true).toBe(true);` and replaces it by something
-    //   that shows a meaningful message when failing (kinda: `success should have been called ...`).
-    //
-    // Contra:
-    // - the spy+callFake construct is not easy to understand on first sight
+
+    it('a bit better', function(done) {
+      var success = jasmine.createSpy('success');
+      promiseThatResolves().then(success).done();
+
+      success.andCallFake(function() {
+        expect(success).toHaveBeenCalled();
+        done();
+      });
+      // Pro
+      // - removes the little meaningful `expect(true).toBe(true);` and replaces it by something
+      //   that shows a meaningful message when failing (kinda: `success should have been called ...`).
+      //
+      // Contra:
+      // - the spy+callFake construct is not easy to understand on first sight
+    });
   });
 
   it('a rejected promise example', function(done) {
