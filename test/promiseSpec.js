d81f4d50c52231f4ffda304dcaf6f5ba81b8f766
Wolfram Kriesing <wk@uxebu.com>
2014-04-21 13:30:42 +0200

    Commented on the first passing promise example.

--- test/promiseSpec.js
+++ test/promiseSpec.js
@@ -6,17 +6,24 @@ describe('setup', function() {
   });
 });
 
-function promise1() {
+function promiseThatResolves() {
   return promise.promise(function(resolve) { resolve(); });
+}
+function promiseThatRejects() {
+  return promise.promise(function(_, reject) { reject('A rejected promise'); });
 }
 
 describe('promises and jasmine', function() {
   it('the simplest passing promise test', function(done) {
-    promise1().then(success);
+    promiseThatResolves().then(success).done();
 
     function success() {
       expect(true).toBe(true);
       done();
     }
+    // Issues here:
+    // - missing `done();` on the last line makes the tests run looong (5s) until the timeout catches
+    //   that would be acceptable, since it kinda gives a hint where to look
+    // - to have the function success at all makes the flow of the test just a bit harder to read
   });
 });
