5278c6042e5454eeba11713a30501d6a39e17eb7
Wolfram Kriesing <wk@uxebu.com>
2014-04-21 13:21:17 +0200

    Add the simplest passing promise.

--- test/promiseSpec.js
+++ test/promiseSpec.js
@@ -1,6 +1,22 @@
-describe('promise and jasmine', function() {
-  it('works fine', function() {
-    expect(true)
-      .toBe(false);
+var promise = require('when');
+
+describe('setup', function() {
+  it('test env works', function() {
+    expect(true).toBe(true);
+  });
+});
+
+function promise1() {
+  return promise.promise(function(resolve) { resolve(); });
+}
+
+describe('promises and jasmine', function() {
+  it('the simplest passing promise test', function(done) {
+    promise1().then(success);
+
+    function success() {
+      expect(true).toBe(true);
+      done();
+    }
   });
 });
